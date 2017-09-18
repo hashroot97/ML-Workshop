e54f54f286d8a94e37dc4fa554084f72068caef8
hashroot97 <roothash97@gmail.com>
2017-09-18 15:22:49 +0530

    Day 3 NNet

---
+++ nnet.R
@@ -0,0 +1,23 @@
+library(neuralnet)
+
+df <- read.csv(file = '/home/bigdata17/Documents/ML-Workshop/concrete.csv', header = TRUE, sep = ',')
+str(df)
+class (df)
+dim(df)
+normalize <- function(x){
+  return ((x - min(x))/ (max(x)-min(x))) 
+}
+concrete_norm = as.data.frame(lapply(df, normalize))
+summary(concrete_norm$strength)
+concrete_train  = df[1:773,]
+concrete_test = df[774:1030,]
+
+concrete_model = neuralnet(strength ~ cement + slag + ash + water + superplastic + coarseagg + fineagg + age,
+                           data=concrete_train)
+plot(concrete_model)
+concrete_model = neuralnet(strength ~ cement + slag + ash + water + superplastic + coarseagg + fineagg + age,
+                           data=concrete_train, hidden = 2)
+plot(concrete_model)
+head(df)
+
+
